27716a70e563c8de555a725fc83b9dfce24414ed
Jimmy Tuck <jtuck15@jimmys-mbp.home>
2020-01-13 21:02:41 +0000

    added social panel with my info

--- index.js
+++ index.js
@@ -21,3 +21,16 @@ async function generateJoke() {
     // add the new joke
     jokeEl.innerHTML = joke.joke;
 }
+
+// SOCIAL PANEL
+var floating_btn = document.querySelector('.floating-btn');
+var close_btn = document.querySelector('.close-btn');
+var social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', function () {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', function () {
+    social_panel_container.classList.remove('visible')
+});
